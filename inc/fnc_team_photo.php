96be93c12ad227016d0eeb88a216e3d898a95ba9
guidoleen <guidoleen@iMac.local>
2017-10-13 17:38:52 +0200

    New image Module

--- inc/fnc_team_photo.php
+++ inc/fnc_team_photo.php
@@ -10,15 +10,21 @@ define('IMG_H', 200);
 // https://bhoover.com/wp_image_editor-wordpress-image-editing-tutorial/
 
     // Id from url parm
-    $Id = $_GET['id'];
+    $Id = absint($_GET['id']);
 
     // Get Image name from metapost
-    $ImgName = get_post_meta($Id, 'photo_name');
+    $ImgName = get_post_meta($Id, 'photo_name', true); // Array
+  
+    // Image info
+    if( file_exists( IMGDIR_TEAM . $ImgName[0] ) )
+        $arrFinfo = getimagesize( IMGDIR_TEAM . $ImgName[0] ); // File size etc.
     
     // Start the Photo upload
     $ImgTag = "";
-    if($ImgName != "") $ImgTag = IMGCONT_TEAM . $ImgName[0];
 
+    if($ImgName[0] != "") $ImgTag = IMGCONT_TEAM . $ImgName[0];
+
+    // Start the upload event
     if( isset($_POST['subfileteam']) )
     {
         $Id = $_GET['id']; // Get id from usrId (post id)
@@ -73,25 +79,35 @@ define('IMG_H', 200);
 
         if( $file['tmp_name'] != null && $file['error'] == 0)
         {
-            $strImgName = strip_str_nospace( get_the_title($Id) );
+            $strImgName = strip_str_nospace( get_the_title($Id) ); // Image from title
             if( $strImgName == "" ) $strImgName = "imgteam" . $Id;
             
                 $strImgName = $strImgName . "." . $fileType;
 
+            // Remove previous file
             if( file_exists(IMGDIR_TEAM . $strImgName) )
             {
-                unlink( IMGDIR_TEAM . $strImgName ); // Remove previous file
-                unlink( IMGDIR_TEAM . "SM" . $strImgName ); // Remove previous SM file
+                unlink( IMGDIR_TEAM . $strImgName );
+            }
+
+            // Remove previous SM file
+            if( file_exists(IMGDIR_TEAM . "SM" . $strImgName) )
+            {
+                unlink( IMGDIR_TEAM . "SM" . $strImgName );
             }
                         
             if( move_uploaded_file($file['tmp_name'], IMGDIR_TEAM . $strImgName ) )
             {
-                update_post_meta($Id, 'photo_name', $strImgName );
                 echo "Gelukt! Jouw foto is geupload...";
             }
+            $arrFinfo = getimagesize( IMGDIR_TEAM . $strImgName ); // File size etc.
 
-            // Imgae info
-            $arrFinfo = getimagesize( IMGCONT_TEAM . $ImgName[0] ); // File size etc.
+             // Do the DB process...
+            $arrImgName = Array();
+            $iAdd = add_up($arrFinfo[5]);
+            array_push( $arrImgName, $strImgName, 0, 0, $arrFinfo[0], $arrFinfo[1], $iAdd ); // Aditional push array
+
+            update_post_meta($Id, 'photo_name', $arrImgName);
         }
     }
 
@@ -103,9 +119,10 @@ function photo_crop($id, $offX, $offY, $imgWidth, $imgHeigth)
     // Start the process...
     try
     {
-        $imgName = get_post_meta($id, 'photo_name'); // Img name
-        $imgSrc = IMGCONT_TEAM . $imgName[0];
-       
+        // Do the Dir process...
+        $imgName = get_post_meta($id, 'photo_name', true); // Img name array
+        $imgSrc = IMGDIR_TEAM . $imgName[0];
+
         $arrFinfo = getimagesize( $imgSrc ); // File size etc.
 
         $img = wp_get_image_editor($imgSrc); // Create imgedit
@@ -113,8 +130,15 @@ function photo_crop($id, $offX, $offY, $imgWidth, $imgHeigth)
 
         $regex = '/[px$]/';
         $img->crop( absint(preg_replace($regex , '', $offX)), absint(preg_replace($regex , '', $offY)), IMG_W, IMG_H ); // $img->crop( $src_x, $src_y, $src_w, $src_h, $dst_w, $dst_h, $src_abs );
-        $filename = IMGDIR_TEAM . "SM" . $imgName[0]; // $img->generate_filename( 'final', IMGCONT_TEAM, NULL );
-        $img->save($filename);
+        $filename = $imgName[0];
+        $dirname = IMGDIR_TEAM . "SM" . $filename; // $img->generate_filename( 'final', IMGCONT_TEAM, NULL );
+        $img->save($dirname);
+
+        // Do the DB process...
+        $arrImgName = Array();
+            array_push($arrImgName, $filename, $offX, $offY, $imgWidth, $imgHeigth, $imgName[5]);
+        update_post_meta($id, 'photo_name', $arrImgName);
+
         echo "Jouw foto is klaar voor gebruik....";
         // $img ->stream();
     }
@@ -124,6 +148,16 @@ function photo_crop($id, $offX, $offY, $imgWidth, $imgHeigth)
     }
     // $img->resize();
 }
+// function adder
+function add_up($iNmr)
+{
+    if( $iNmr < 10)
+        return $iNmr + 1;
+    else
+        return 0;
+}
+
+// Posting the avatar
 if( isset($_POST['savecrop']) )
 {
     $offX = $_POST['offX'];
@@ -166,8 +200,17 @@ if( isset($_POST['savecrop']) )
         <!-- <div id="cont_team_show">
             <img src="<?php echo $ImgTag ?>"> />
         </div> -->
-        <div id="img_team_show" style="background-image: url( <?php echo $ImgTag ?> ); width:<?php echo IMG_W ?>; height:<?php echo IMG_H ?>;"></div>
-        <input type="range" id="rngPerc" min="0" max="100" step="1" list="tickmarks" />
+        <div id="img_team_show" 
+            style="background-image: url( <?php echo $ImgTag ?> ); 
+            width:<?php echo IMG_W ?>; 
+            height:<?php echo IMG_H ?>;
+            background-size: <?php echo $ImgName[3] . "px " . $ImgName[4] . "px" ?>;
+            background-position-x: <?php echo $ImgName[1] ?>;
+            background-position-y: <?php echo $ImgName[2] ?>;
+        ">
+        </div>
+
+        <input type="range" id="rngPerc" min="0" max="100" step="1" list="tickmarks" start="100" />
             <datalist id="tickmarks">
             <option value="0" label="0%">
             <option value="10">
@@ -209,3 +252,13 @@ if( isset($_POST['savecrop']) )
   'bits' => int 8
   'channels' => int 3
   'mime' => string 'image/jpeg' (length=10) -->
+
+  <!-- // array (size=1)
+        // 0 => 
+        //   array (size=5)
+        //     0 => string 'NicolettevanPutten.jpeg' (length=23)
+        //     1 => string '-44px' (length=5)
+        //     2 => string '-1px' (length=4)
+        //     3 => string '294.40000000000003' (length=18)
+        //     4 => string '220.8' (length=5)
+        //      5 => int 1 -->
